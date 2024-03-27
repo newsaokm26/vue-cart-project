fb11f77dfe15f012a577a6348ab7a63624e0f8e3
newsakm26 <newsakm26@gmail.com>
2024-03-27 14:32:48 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/MengtingKu/vueTest.git main:gh-pages
+git push -f https://github.com/newsaokm26/vue-cart-project main:gh-pages
 
 cd -
