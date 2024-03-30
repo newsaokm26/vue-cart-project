8dec80a230718e35340087808f2c19937dce1ce5
newsakm26 <newsakm26@gmail.com>
2024-03-30 14:29:44 +0800

    返回頂部動畫

--- src/components/AppGotop.vue
+++ src/components/AppGotop.vue
@@ -5,13 +5,26 @@ import { useScroll } from '@vueuse/core'
 const { y } = useScroll(window)
 
 // 返回頂部
-const goTop = () => {
-  document.documentElement.scrollTop = 0
+const scollTop = () => {
+  // 設置定時器
+  const upRoll = setInterval(() => {
+    // 每次獲取頁面被捲去的部分
+    const top = document.documentElement.scrollTop
+    // 每次滾動多少
+    const speed = Math.ceil(top / 10)
+    if (document.documentElement.scrollTop > 0) {
+      // 不在頂部 每次滾動到的位置
+      document.documentElement.scrollTop -= speed
+    } else {
+      // 返回頂部清除定時器
+      clearInterval(upRoll)
+    }
+  }, 10)
 }
 </script>
 <template>
   <div class="app-evevator" :class="{ show: y > 300 }">
-    <el-icon @click="goTop"><ArrowUp /></el-icon>
+    <el-icon @click="scollTop"><ArrowUp /></el-icon>
   </div>
 </template>
 <style scoped lang="scss">
