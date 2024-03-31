ed4dbfcecc3f6836b16363248f2ec3d1eab3a8d1
newsakm26 <newsakm26@gmail.com>
2024-03-31 23:21:17 +0800

    輪播圖調整

--- src/views/home/components/HomeBanner.vue
+++ src/views/home/components/HomeBanner.vue
@@ -1,27 +1,31 @@
 <script setup>
 // import { ref } from 'vue'
 
-// const imgList = ref([
-//   {
-//     id: 1,
-//     imgUrl: '@/assets/banner/banner01.png'
-//   },
-//   {
-//     id: 2,
-//     imgUrl: '@/assets/banner/banner02.png'
-//   }
-// ])
-
-const testCl = (item) => {
-  console.log(item)
-}
+const homeCarousel = [
+  {
+    id: '1001001000',
+    name: '寶可夢 | 精靈球',
+    imgUrl:
+      'https://images.unsplash.com/photo-1613771404721-1f92d799e49f?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: '1001001001',
+    name: '寶可夢 | 大頭公仔',
+    imgUrl:
+      'https://images.unsplash.com/photo-1605979257913-1704eb7b6246?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  }
+]
 </script>
 
 <template>
   <div class="home-banner">
     <el-carousel height="500px">
-      <el-carousel-item v-for="item in 4" :key="item" @click="testCl(item)">
-        <img src="@/assets/banner/banner01.png" alt="" />
+      <el-carousel-item
+        v-for="item in homeCarousel"
+        :key="item.id"
+        @click="$router.push(`/detail/manage/${item.id}`)"
+      >
+        <img :src="item.imgUrl" alt="" />
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -37,6 +41,7 @@ const testCl = (item) => {
     width: 100%;
     height: 500px;
     object-fit: cover; // 等比例縮放
+    cursor: pointer;
   }
 }
 </style>
